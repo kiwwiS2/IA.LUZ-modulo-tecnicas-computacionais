3a312401eed5207e81503f96e6108a6853079c58
kiwi <106825312+kiwwiS2@users.noreply.github.com>
2024-09-10 11:56:52 -0300

    Update main.js

--- iA.LUZ-main-modulo tecnicas computacionais/main.js
+++ iA.LUZ-main-modulo tecnicas computacionais/main.js
@@ -1,10 +1,13 @@
+import { aleatorio, nome } from './aleatorio.js';
+import { perguntas } from './perguntas.js';
+
 const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 
-
+const botao)Jogarnovamente
 
 const perguntas = [
     {
