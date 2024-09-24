357a7a2ebb4e350bc11159ab8f7c23ceaaa0ec37
kiwi <106825312+kiwwiS2@users.noreply.github.com>
2024-09-24 11:42:00 -0300

    Update aleatorio.js

--- js/aleatorio.js
+++ js/aleatorio.js
@@ -1,1 +1,8 @@
-const nomes = [Thomas, Eduarda, Vitoria, Antonella, apollo,saimon];
+const nomes = [Thomas, Eduarda, Vitoria, Antonella, apollo ,saimon , cristal];
+
+export function aleatorio (lista){
+    const posicao = Math.floor(Math.random()* lista.length);
+    return lista[posicao];
+}
+
+export const nome = aleatorio(nomes)
